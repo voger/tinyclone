42ac81cde5890a5b2739d1841292fdb40b137b69
voger <alktsam@yahoo.gr>
2019-09-03 23:33:34 +0300

    Add validation for url's with scheme

--- tinyclone_backend/test/tinyclone/shortener_test.exs
+++ tinyclone_backend/test/tinyclone/shortener_test.exs
@@ -9,6 +9,8 @@ defmodule TinyClone.ShortenerTest do
     :ok
   end
 
+  @uris [com: "http://www.example.com", net: "http://www.example.net", invalid: "www.example.com"]
+
   @doc """
   Helper function to reset the id sequence of table `urls` to
   a specific number
@@ -20,32 +22,39 @@ defmodule TinyClone.ShortenerTest do
   end
 
   test "create new link without custom" do
-    assert {:ok, "" <> _} = Sh.shorten("www.example.com")
+    assert {:ok, "" <> _} = Sh.shorten(@uris[:com])
   end
 
   test "create new link with custom" do
     custom = "example"
-    assert {:ok, ^custom} = Sh.shorten("www.example.com", custom)
+    assert {:ok, ^custom} = Sh.shorten(@uris[:com], custom)
+  end
+
+  test "create new link with invalid url fails" do
+    invalid_url = @uris[:invalid]
+    {:error, :url, %Ecto.Changeset{} = changeset} = Sh.shorten(invalid_url)
+    assert "#{invalid_url} is not a valid url" in errors_on(changeset).original
   end
 
   test "create new link with custom bad word fails" do
     custom = "shit"
-    {:error, :link, changeset} = Sh.shorten("www.example.com", custom)
+    {:error, :link, %Ecto.Changeset{} = changeset} = Sh.shorten(@uris[:com], custom)
 
     assert "#{custom} is a bad word" in errors_on(changeset).identifier
   end
 
   test "create custom link that clashes with generated link fails" do
-    {:ok, link} = Sh.shorten("www.example.com")
+    {:ok, link} = Sh.shorten(@uris[:com])
 
-    {:error, :link, changeset} = Sh.shorten("www.example.net", link)
+    {:error, :link, %Ecto.Changeset{} = changeset} = Sh.shorten(@uris[:net], link)
 
     assert "has already been taken" in errors_on(changeset).identifier
   end
-  test "create custom link that clashes with custom link fails" do
-    Sh.shorten("www.example.com", "example")
 
-    {:error, :link, changeset} = Sh.shorten("www.example.net", "example")
+  test "create custom link that clashes with custom link fails" do
+    Sh.shorten(@uris[:com], "example")
+
+    {:error, :link, %Ecto.Changeset{} = changeset} = Sh.shorten(@uris[:net], "example")
 
     assert "has already been taken" in errors_on(changeset).identifier
   end
@@ -55,7 +64,7 @@ defmodule TinyClone.ShortenerTest do
     {:ok, bad_id} = Encoder.decode(bad_link)
     reset_urls_id(bad_id)
 
-    {:ok, link} = Sh.shorten("www.example.com")
+    {:ok, link} = Sh.shorten(@uris[:com])
 
     url_id =
       Repo.one(
@@ -71,11 +80,11 @@ defmodule TinyClone.ShortenerTest do
   end
 
   test "sequence creates a link that clashes with a custom link, ignores it and creates a new link" do
-    {:ok, bad_link} = Sh.shorten("www.example.com", "example")
+    {:ok, bad_link} = Sh.shorten(@uris[:com], "example")
 
     {:ok, bad_id} = Encoder.decode("example")
     reset_urls_id(bad_id)
-    {:ok, link} = Sh.shorten("www.example.net")
+    {:ok, link} = Sh.shorten(@uris[:net])
 
     url_id =
       Repo.one(
